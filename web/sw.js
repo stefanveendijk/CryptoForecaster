c7df835c4c6ac318a00d0dea58c4ba61a7e87dbf
stefanveendijk <stefanveendijk@gmail.com>
2026-09-13 10:07:50 +0200

    Add PWA service worker

---
+++ web/sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'crypto-forecaster-v1';
+const SHELL = ['/', '/manifest.webmanifest', '/static/icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  if (url.pathname.startsWith('/api/')) {
+    event.respondWith(
+      fetch(req).catch(() => new Response(JSON.stringify({offline:true}), {
+        status: 503,
+        headers: {'Content-Type': 'application/json'}
+      }))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(req)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(cache => cache.put(req, copy));
+        return res;
+      })
+      .catch(() => caches.match(req).then(hit => hit || caches.match('/')))
+  );
+});
